51255ab85a56f823d25c1adb3bd0ce7fe734f4fa
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-01-19 01:33:06 +1100

    Updated gitignore and prettier options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
-module.exports =  {
-  semi:  true,
-  trailingComma:  'all',
-  singleQuote:  true,
-  printWidth:  120,
-  tabWidth:  4,
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    jsxBracketSameLine: true,
+    arrowParens: 'always',
+    parser: 'typescript',
 };
