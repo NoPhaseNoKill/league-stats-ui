a3cb8b4ce1de6d0e2f40e2c1b4b48bb9a1180670
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-01-19 01:07:57 +1100

    Setup base project

---
+++ src/app/components/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GridColumn } from 'semantic-ui-react'
+
+export class HomePage extends React.Component {
+  render () {
+    return (
+      <div>
+        <GridColumn/>
+        Hello World
+      </div>
+    )
+  }
+}
