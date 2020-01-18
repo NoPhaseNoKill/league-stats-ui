7568c326d05e3377dba45ac45875255966826dee
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-01-19 01:57:27 +1100

    Updated tab spacing for prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    semi: true,
-    trailingComma: 'all',
-    singleQuote: true,
-    printWidth: 120,
-    tabWidth: 4,
-    jsxBracketSameLine: true,
-    arrowParens: 'always',
-    parser: 'typescript',
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  parser: 'typescript',
 };
--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { GridColumn } from 'semantic-ui-react';
 
 export class HomePage extends React.Component {
-    render() {
-        return (
-            <div>
-                <GridColumn />
-                Hello World
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <GridColumn />
+        Hello World
+      </div>
+    );
+  }
 }
