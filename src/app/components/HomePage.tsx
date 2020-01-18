d83a83167c24a1154b28ce17b09710a60a98dba0
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-01-19 01:33:06 +1100

    Added basic prettier and eslint

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { GridColumn } from 'semantic-ui-react'
+import React from 'react';
+import { GridColumn } from 'semantic-ui-react';
 
 export class HomePage extends React.Component {
-  render () {
-    return (
-      <div>
-        <GridColumn/>
-        Hello World
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <GridColumn />
+                Hello World
+            </div>
+        );
+    }
 }
