2edf4a0b8b508473f3b09ef817a993ec1a891be2
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-01-25 19:41:02 +1100

    Removed Steam API key

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const requestHandlers = [cors(), bodyParser.json()];
 app.use(requestHandlers);
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-const STEAM_API_KEY = '5BC5AD8A2E84580B4B96EB2ED511384B';
+const STEAM_API_KEY = '';
 
 app.get('/*', (req, res) => {
   const url = `https://api.steampowered.com${req.originalUrl}/?key=${STEAM_API_KEY}`;
