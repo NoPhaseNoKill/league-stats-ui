4aa906035377c10c2c4224815d2a2d866fa71d07
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-01-26 11:53:25 +1100

    Add .env file to handle API key usage

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,8 @@ const requestHandlers = [cors(), bodyParser.json()];
 app.use(requestHandlers);
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-const STEAM_API_KEY = '';
-
 app.get('/*', (req, res) => {
-  const url = `https://api.steampowered.com${req.originalUrl}/?key=${STEAM_API_KEY}`;
+  const url = `https://api.steampowered.com${req.originalUrl}/?key=${process.env.STEAM_API}`;
   request.get(url, (error, steamRes, steamBody) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(steamBody);
