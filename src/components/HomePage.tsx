a5854c6f010377cdbec3b1cc734a92d383e09a5b
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-01-27 16:38:58 +1100

    Remove console logs and set hero and items to state of HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import { Banner } from 'components/Banner';
 import { HeroService } from 'services/HeroService';
 import { ItemService } from 'services/ItemService';
+import { Hero } from 'types/Hero/Hero';
+import { Item } from 'types/Item/Item';
+
+type State = {
+  heroes: Hero[];
+  items: Item[];
+};
 
 export class HomePage extends React.PureComponent {
+  state: State = {
+    heroes: [],
+    items: [],
+  };
+
   componentDidMount = async () => {
     const heroes = await HeroService.getAll();
     const items = await ItemService.getAll();
-    console.log('items', items);
-    console.log('heroes', heroes);
+    this.setState({ heroes, items });
   };
 
   render() {
