28c43f7a5a51b8861c9617ac566097bcb2d14c2d
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-01-31 22:11:58 +1100

    Added specific semantic strict props to ensure intellisense remains when using generic ThemedComponentProps

--- src/types/Theme/ThemedComponentProps.ts
+++ src/types/Theme/ThemedComponentProps.ts
@@ -1,5 +1,23 @@
 import { DefaultTheme } from 'styled-components';
+import {
+  StrictButtonContentProps,
+  StrictButtonGroupProps,
+  StrictButtonOrProps,
+  StrictButtonProps,
+} from 'semantic-ui-react';
 
-export type ThemedComponentProps<T> = T & {
+/*
+  Used to ensure intellisense/code completion remains on generic semantic components
+  Please add the equivalent group (ie Button/Container/List etc) if you need to use one or more
+ */
+
+type SemanticStrictButtonProps =
+  | StrictButtonProps
+  | StrictButtonContentProps
+  | StrictButtonGroupProps
+  | StrictButtonOrProps;
+type SemanticStrictComponentProps = SemanticStrictButtonProps;
+
+export type ThemedComponentProps<T extends SemanticStrictComponentProps> = T & {
   theme: DefaultTheme;
 };
