9acdd6bf51e12edba189595e75b140aba411e22d
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-03-07 20:03:48 +1100

    Configured enzyme, jest, and typescript testing framework

---
+++ src/tests/App/components/Navigation/Navigation.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { NavigationButton } from 'styles/components/Buttons';
+import App from 'App';
+
+it('renders navigation buttons when app starts', () => {
+  const wrapper = mount(<App />);
+  const navigationButtons = wrapper.find(NavigationButton);
+  expect(wrapper.containsMatchingElement(<NavigationButton route={'/'} label={'Home'} />)).toBeTruthy();
+  expect(wrapper.containsMatchingElement(<NavigationButton route={'/heroes'} label={'Heroes'} />)).toBeTruthy();
+  expect(navigationButtons).toHaveLength(2);
+});
