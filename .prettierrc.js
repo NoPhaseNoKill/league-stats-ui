a813db4c8bebdb154ea69d32a56be6b5ab2d9789
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-03-29 17:33:21 +1100

    Altered prettier config to rely on defaults where possible

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,6 @@
 module.exports = {
-  semi: true,
   trailingComma: 'all',
   singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
-  jsxBracketSameLine: true,
   arrowParens: 'always',
   bracketSpacing: true,
 };
