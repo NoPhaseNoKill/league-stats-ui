961905d2696d09d78fea456e66c53f055fb1a20b
Tom Gardiner <tom.gardiner@ioof.com.au>
2020-04-11 22:53:15 +1000

    Remove react-app-rewired. No longer needed as this was for workaround with semantic ui react and styled components playing nicely

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const { useBabelRc, override } = require('customize-cra');
-
-/*
-  Used to allow babel plug-ins without having to eject create-react-app
- */
-
-module.exports = override(useBabelRc());
